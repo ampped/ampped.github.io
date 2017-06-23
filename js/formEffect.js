08a0095d8b03c840cc370044657a3d1d89b70be2
Amy Pham <amp4129@g.rit.edu>
2017-06-22 21:26:00 -0700

    6/22 update 3
    fixed Cs, updated required effect, added webkits

--- js/formEffect.js
+++ js/formEffect.js
@@ -25,13 +25,19 @@ function formEffect(){
 
 		//insert * to placeholder for required 
 		var placeholderString = placeholder.children('p').get(0).innerText;
-		if(placeholderString.charAt(placeholderString.length - 1) != "*")
+		if(placeholderString.charAt(placeholderString.length - 1) != "*"){
 			jQuery(placeholder).children('p').append('*');
+			jQuery(placeholder.children('p').get(0)).css('color', '#FF4B54');
+			jQuery(this).offsetParent().css('border-bottom', '2px solid #FF4B54');
+		}
 
-		if(jQuery(this).val().length > 0)
+		if(jQuery(this).val().length > 0){
 			placeholder.addClass('notEmpty');
+			jQuery(this).offsetParent().removeAttr('style');
+		}
 		else{
 			placeholder.removeClass('notEmpty');
+			jQuery(this).offsetParent().css('border-bottom', '2px solid #FF4B54');
 		}
 		//border
 		var effect = jQuery(this).prev();
