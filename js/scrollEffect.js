a2a7ea9ff1bd4249aa01bc20e32c35bcf32f2019
Amy Pham <amp4129@g.rit.edu>
2018-03-29 02:53:48 -0400

    creative industry day update

--- js/scrollEffect.js
+++ js/scrollEffect.js
@@ -3,6 +3,13 @@ function scrollEffect(){
 
 	jQuery(window).scroll(function(e){
 		hideNav(e);
+
+    if(jQuery(e.target).scrollTop() > 600){
+      jQuery('nav').addClass('solidBG');
+    }
+    else{
+      jQuery('nav').removeClass('solidBG');
+    }
 	});
 	jQuery('.projectBlock').scroll(function(e){
 		hideNav(e);
@@ -10,12 +17,12 @@ function scrollEffect(){
 
 	function hideNav(e){
 		var st = jQuery(e.target).scrollTop();
-		if(st > 100){
+		/*if(st > 100){
 			jQuery('.logo').attr('class', 'logo shrink');
 		}
 		else if(lastScrollTop > 100 && st < 100){
 			jQuery('.logo').attr('class', 'logo grow');
-		}
+		}*/
 
 	    if (st < lastScrollTop){
 			jQuery('nav').addClass('showing');
@@ -27,6 +34,46 @@ function scrollEffect(){
 
 // Select all links with hashes
 jQuery('nav>a')
+  // Remove links that don't actually link to anything
+  .not('[href="resume.pdf"]')
+  .click(function(event) {
+
+    if(jQuery('.projectBlock showing')){
+    jQuery('.projectBlock').removeClass('showing');
+      setTimeout(function(){
+      jQuery('body').css('overflow-y', 'scroll');
+      }, 500);
+  }
+    // On-page links
+
+    //code modified from https://css-tricks.com/snippets/jquery/smooth-scrolling/
+      // Figure out element to scroll to
+      var target = jQuery(this).attr('data-link');
+      target = jQuery(target);
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        jQuery('html, body').animate({
+          scrollTop: target.offset().top
+        }, 600, 'swing', function() {
+          // Callback after animation
+          // Must change focus!
+          var jQuerytarget = jQuery(target);
+          jQuerytarget.focus();
+          if (jQuerytarget.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            jQuerytarget.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            jQuerytarget.focus(); // Set focus again
+          };
+        });
+      }
+  });
+
+
+// Select all links with hashes
+jQuery('.mobileNav>a')
   // Remove links that don't actually link to anything
   .not('[href="resume.pdf"]')
   .click(function(event) {
--- js/wireframeDisplay.js
+++ js/wireframeDisplay.js
@@ -1,19 +1,5 @@
 //written by Amy Pham
 
-function addSquiggles(){
-	jQuery('h1').append('<br><svg xmlns="http://www.w3.org/2000/svg" width="208" height="14" viewBox="0 0 207.6 13.6"><style type="text/css">' +
-		'.st0{fill:none;stroke:#D1F084;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}'+
-	'</style><path class="st0" d="M206.6 1c-7.5 0-10.1 3.4-12.9 5.8 -3 2.6-5.3 5.8-12.9 5.8 -7.5 0-9.9-3.2-12.9-5.8 -2.8-2.4-5.3-5.8-12.9-5.8 -7.5 0-9.9 3.2-12.9 5.8 -2.8 2.4-5.3 5.8-12.9 5.8 -7.5 0-10.1-3.4-12.9-5.8 -3-2.6-5.3-5.8-12.9-5.8S93.8 4.4 91 6.8c-3 2.6-5.3 5.8-12.9 5.8s-9.9-3.2-12.9-5.8C62.5 4.4 59.9 1 52.4 1S42.3 4.4 39.6 6.8c-3 2.6-5.3 5.8-12.9 5.8s-9.9-3.2-12.9-5.8C11.1 4.4 8.5 1 1 1"/></svg>');
-	jQuery('h3').each(function(){
-		jQuery(this).prepend('<svg xmlns="http://www.w3.org/2000/svg" width="131" height="24" viewBox="0 0 130.5 13.6"><style type="text/css">'+  
-		'.st0{fill:none;stroke:#D1F084;stroke-width:2;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}'+
-	'</style><path class="st0" d="M129.5 12.6c-7.5 0-10.1-3.4-12.9-5.8 -3-2.6-5.3-5.8-12.9-5.8S93.8 4.4 91 6.8c-3 2.6-5.3 5.8-12.9 5.8s-9.9-3.2-12.9-5.8C62.5 4.4 59.9 1 52.4 1S42.3 4.4 39.6 6.8c-3 2.6-5.3 5.8-12.9 5.8s-9.9-3.2-12.9-5.8C11.1 4.4 8.5 1 1 1"/></svg>&nbsp;&nbsp;&nbsp;');
-		jQuery(this).append('&nbsp;&nbsp;&nbsp;<svg xmlns="http://www.w3.org/2000/svg" width="131" height="24" viewBox="0 0 130.5 13.6"><style type="text/css">'+  
-		'.st0{fill:none;stroke:#D1F084;stroke-width:3;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:10;}'+
-	'</style><path class="st0" d="M1 12.6c7.5 0 10.1-3.4 12.9-5.8 3-2.6 5.3-5.8 12.9-5.8s10.1 3.4 12.9 5.8c3 2.6 5.3 5.8 12.9 5.8s9.9-3.2 12.9-5.8C68 4.4 70.6 1 78.1 1S88.2 4.4 91 6.8c3 2.6 5.3 5.8 12.9 5.8s9.9-3.2 12.9-5.8C119.5 4.4 122 1 129.5 1"/></svg>');
-	});
-}
-
 function setupWFDisplay(wfSrcs){
 	jQuery('.pages>li').click(function(){
 		jQuery('.pages>.selected').removeClass('selected');
