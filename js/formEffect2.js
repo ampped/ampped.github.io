ec9acc4d314a37d8bbbd4cc9cdbce7a73b0dceed
Amy Pham <amp4129@g.rit.edu>
2018-12-11 23:02:42 -0500

    FORM EFFECT NOT SCROLL EFFECT

---
+++ js/formEffect2.js
@@ -0,0 +1,61 @@
+function formEffect(){
+	var inputs = jQuery('.form-control');
+
+
+	jQuery(inputs).each(function(){
+		jQuery(this).keyup(function(){
+			//notify user the field is required if left empty
+			if(jQuery(this).val().length > 0){
+				jQuery(this).next().addClass('notEmpty');
+				jQuery(this).removeAttr('style');
+			}
+			else{
+				jQuery(this).next().removeClass('notEmpty');
+			}
+
+			//disable/enable submit button
+			if(jQuery('.notEmpty').length == inputs.length)
+				jQuery('.disabled').removeClass('disabled');
+			else
+				jQuery('.form-submit-button').addClass('disabled');
+		});
+
+		var placeholder = jQuery(this).attr('placeholder');
+		if(placeholder != undefined){
+			jQuery(this).after('<div class="label"><label for="' + jQuery(this).attr('id') + '">' + placeholder + '</label></div>');
+			jQuery(this).after('<div class="placeholder"><p>' + placeholder + '</p></div>');
+			jQuery(this).removeAttr('placeholder');
+		}
+	});
+
+	inputs.focus(function(){
+		selectedGroup = jQuery(this).offsetParent();
+		//border
+		selectedGroup.prepend('<div class="control-border"></div>');
+		jQuery(this).prev().animate({
+		    width: "100%"
+		}, 15, 'linear');
+	});
+
+	inputs.blur(function(){
+		//placeholder
+		var placeholder = jQuery(this).next();
+
+		//insert * to placeholder for required 
+		var placeholderString = placeholder.children('p').get(0).innerText;
+		if(placeholderString.charAt(placeholderString.length - 1) != "*"){
+			jQuery(placeholder).children('p').append('<span class="specialC">*</span>');
+		}
+
+		if(jQuery(this).val().length <= 0)
+			jQuery(this).css('border-bottom', '3px solid #FF4B54');
+
+		//border
+		var effect = jQuery(this).prev();
+		effect.css({'right': '0', 'width': '0'});
+
+		window.setTimeout(function(){
+			effect.remove();
+		}, 500);
+	});
+}
